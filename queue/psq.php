bd78a281f90aba3e5fef5ea61024e4c9cfca2bc7
DebianDan <tucholski.4@osu.edu>
2013-01-11 18:09:10 -0500

    updated psq

---
+++ queue/psq.php
@@ -0,0 +1,10 @@
+<?php
+// generic registration stuff here.
+
+if( !file_exists( 'config.php' ) ){
+    die( 'Config file doesn\' exist.  Did you forget to copy config.php.default to config.php?');
+}
+
+require_once( 'config.php' );
+
+$DB = new mysqli( DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE );
