3855f8b6221cb7842cff0e75012b34cb4f1b7430
DebianDan <tucholski.4@osu.edu>
2013-01-11 18:24:18 -0500

    structured queue folder

--- queue/PS/index.php
+++ queue/PS/index.php
@@ -9,8 +9,19 @@
 	<h1>Hello, world!</h1>
 	<script src="http://code.jquery.com/jquery-latest.js"></script>
 	<script src="/js/bootstrap.min.js"></script>
-	
-	<?php include 'psq.php'; ?>
-	
+
+	<?php
+		// generic registration stuff here.
+
+	if( !file_exists( '../../config.php' ) ){
+		die( 'Config file doesn\' exist.  Did you forget to copy config.php.default to config.php?');
+	}
+
+		require_once( '../../config.php' );
+
+	$DB = new mysqli( DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE );
+
+	?>
+
 </body>
 </html>
