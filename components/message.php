377beabaaa6d08d607fa453ab150c7ca11f10d3f
Thach Hoang <thach@thachhoang.net>
2013-01-12 12:39:17 -0800

    S3 img opened in browser, message component.

---
+++ components/message.php
@@ -0,0 +1,11 @@
+<!--
+Add these:
+<link href="/css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+PHP:
+$message = "Yeah.";
+include 'message.php';
+-->
+<div style="width:100%; text-align:center; padding-top:3em;">
+	<h2><?php echo $message; ?></h2>
+</div>
