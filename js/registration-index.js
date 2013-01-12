917ade7f09560aa358d2b68ed0e28a59017d6438
Salvatore Testa <sal.testa@gmail.com>
2013-01-11 23:18:08 -0800

    registration now redirects to welcome.html

--- js/registration-index.js
+++ js/registration-index.js
@@ -58,19 +58,7 @@ function showRequest(formData, jqForm, options) {
 
 // post-submit callback 
 function showResponse(responseText, statusText, xhr, $form)  { 
-    // for normal html responses, the first argument to the success callback 
-    // is the XMLHttpRequest object's responseText property 
- 
-    // if the ajaxForm method was passed an Options Object with the dataType 
-    // property set to 'xml' then the first argument to the success callback 
-    // is the XMLHttpRequest object's responseXML property 
- 
-    // if the ajaxForm method was passed an Options Object with the dataType 
-    // property set to 'json' then the first argument to the success callback 
-    // is the json data object returned by the server 
- 
-    alert('status: ' + statusText + '\n\nresponseText: \n' + responseText + 
-        '\n\nThe output div should have already been updated with the responseText.'); 
+	window.location = '/registration/welcome.html';
 } 
 
 /*
