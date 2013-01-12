28fda73fa2e6b33cf0b5f7e4a8b27f7ad3318edf
Matt McNamara <matt@expensify.com>
2013-01-11 22:22:02 -0800

    Typo fix in _before.php

--- _before.php
+++ _before.php
@@ -4,7 +4,7 @@ if( isset( $_REQUEST['pgid'] ) && intval( $_REQUEST['pgid'] ) != $_REQUEST['pgid
     fatalErrorContactMatt( 'Invalid pgid.' );
 }
 
-if( isset( $_REQUEST['token'] ) && preg_match( '/[A-Z]{6}/', $_REQUEST['token'] ) ){
+if( isset( $_REQUEST['token'] ) && preg_match( '/[A-Z]{6}/', $_REQUEST['token'] ) != 1 ){
     fatalErrorContactMatt( 'Invalid Token.' );
 }
 
