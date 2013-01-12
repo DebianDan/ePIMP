9b2adfa3967142c36fbdfb2de280180bac5a2556
Matt McNamara <matt@expensify.com>
2013-01-11 19:25:48 -0800

    Update .gitignore and config with s3 credentials.

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+define( 'DB_HOST', 'db.expensiparty.com' );
+define( 'DB_DATABASE', 'expensiparty' );
+define( 'DB_USER', 'expensiparty_dev' );
+define( 'DB_PASSWORD', 'asdfASDF00' );
+
+define( 'S3_USERNAME' , 'expensiparty_s3_user' );
+define( 'S3_ACCESS_KEY_ID', 'AKIAJ4I5MTGDDJ5N2AVA' );
+define( 'S3_SECRET_ACCESS_KEY', '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' );
+
