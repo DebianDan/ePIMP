1f772a34d9769ab412c1c496fc601bdde9095e77
Eric Jang <ericjang2004@gmail.com>
2013-01-12 12:59:39 -0800

    fixed lagging message so that it doesnt come after post

--- dashboard/userProfile.php
+++ dashboard/userProfile.php
@@ -28,7 +28,7 @@ $friend = get_user_info($friendID);
       echo "<p>" . $friend['intro'] . "</p>";
       echo "<a href='#' id='addfriend' data-role='button' data-icon='check'>I have met this person!</a>"
     ?>
-    <a href="index.php" data-role="button" data-theme="c" data-rel="back">Go Back</a>
+    <a href="index.php" data-role="button" data-theme="c">Go Back</a>
 	  <p id="messages">
     </p>
   </div>
@@ -36,14 +36,13 @@ $friend = get_user_info($friendID);
 <script>
 $('#addfriend').click(function(){
   $(this).remove();
+  $('#messages').text('Congratulations! Make sure your new friend clicks your name right away, or else you won\'t be awarded points.');
   <?php
   //use php to render part of the script
     echo "var data = 'pgid=" . $pgid . "&token=" . $token . "&friendID=" . $friend['pgid'] . "&mingle_status_pk=" . $_REQUEST['mingle_status_pk'] . "'";
     echo "\n"
   ?>
-  $.post('update_friendship.php?'+data, function(res) {
-    $('#messages').text('Congratulations! Make sure your new friend clicks your name right away, or else you won\'t be awarded points.');
-  });
+  $.post('update_friendship.php?'+data, function(res) {});
 })
 </script>
 
--- realtime_feed/index.php
+++ realtime_feed/index.php
@@ -8,12 +8,12 @@
     <link href="/css/bootstrap.min.dark.css" rel="stylesheet" media="screen">
   </head>
   <body>
-    <h1 style="text-align: center">P.I.M.P. REAL-TIME FEED</h1>
+    <h1 style="text-align: center">A PIMPIN' REAL-TIME FEED</h1>
     <script src="/js/jquery-latest.js"></script>
     <script src="/js/bootstrap.min.js"></script>
     <div class="row-fluid">
       <div class="span8 offset2">
-        <table id="feed" class="table .table-striped">
+        <table id="feed" class="table .table-striped .table-condensed">
           <tr>
             <th>Time</th>
             <th>Name</th>
@@ -33,9 +33,9 @@
       console.log(data);
       if (window.last !== data.time) {
         if (data.points > 50) {
-          var img = "/img/down_red_arrow.png";
-        } else {
           var img = "/img/up_green_arrow.png";
+        } else {
+          var img = "/img/down_red_arrow.png";
         }
         var formatted_time = data.time
         $('#feed').append('<tr><td>' + formatted_time + '</td><td>' + data.first_name + ' ' + data.last_name + '</td><td><img src="'+ img + '"</td><td>' + data.points + '</td><td>' + data.reason +'</td></tr>');
