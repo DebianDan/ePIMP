3a0e3330eeb2e4575c92ddc3788e765cc427917a
Matt McNamara <matt@expensify.com>
2013-01-12 03:12:35 -0800

    Send mail stuff.

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ require_once( 'config.php' );
 
 //$DB = new mysqli( DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE );
 
+email_person( 1, 'Welcome', array( 'name' => 'Matt', 'url' => 'http://expensiparty.com/?pgid=1287316&token=FPAKJZ' ) );
+die(' Done here.' );
 $cookie = $_REQUEST['dispatcher'];
 $app = 'dashboard';
 
