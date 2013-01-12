41047645936b7487622a4eef4ecb444c4aff62e5
Salvatore Testa <sal.testa@gmail.com>
2013-01-12 04:42:39 -0800

    added pkid to the get that is passed to the the roulette from the dashboard

--- dashboard/index.php
+++ dashboard/index.php
@@ -13,7 +13,7 @@ if ($play_mingle == 0) {
 <head>
   <title>User Dashboard</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="/css/jquery.mobile.min.css"/>
+  <link rel="stylesheet" href="/css/jquery.mobile.css"/>
   <script src="/js/jquery-latest.js"></script>
   <script src="/js/jquery.mobile-1.2.0.min.js"></script>
 </head>
@@ -104,7 +104,8 @@ if ($play_mingle == 0) {
       <ul data-role="listview" data-inset="true">
         <li>
           <?php
-            echo "<a href='/dashboard/roulette.php?pgid=" . $pgid . "&token=" . $token . "' data-transition='slide' data-ajax='false'>Play Roulette</a>";
+            $account = get_user_info($pgid); 
+            echo "<a href='/dashboard/roulette.php?pgid=" . $pgid . "&token=" . $token . "&pkid=".$account['accounts_pk']."' data-transition='slide' data-ajax='false'>Play Roulette</a>";
           ?>
         </li>
       </ul>
