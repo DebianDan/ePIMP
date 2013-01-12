395a8924d1edf877b3637cea66d31401a0f7a023
Salvatore Testa <sal.testa@gmail.com>
2013-01-11 17:16:51 -0800

    Registration now takes in pk_id and passes to the backend

---
+++ js/registration-index.js
@@ -0,0 +1,11 @@
+var get = [], hash;
+var q = document.URL.split('?')[1];
+if(q != undefined){
+	q = q.split('&');
+	for(var i = 0; i < q.length; i++){
+		hash = q[i].split('=');
+		get.push(hash[1]);
+		get[hash[0]] = hash[1];
+	}
+}
+$('#pkID').val(get['pk_id']);
