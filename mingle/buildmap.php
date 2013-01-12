7647416fe1a9ca3e4536c80a00855719a8e9e963
Arapat Jurat <arapat@arap.at>
2013-01-11 20:36:23 -0800

    mingle system backend

---
+++ mingle/buildmap.php
@@ -0,0 +1,60 @@
+<?php
+$con = mysql_connect("localhost", "mysql_user", "mysql_password") or
+    die("Could not connect: " . mysql_error());
+mysql_select_db("pimp", $con);
+
+/*
+Get the list of all users
+*/
+$result = mysql_query("SELECT pgid FROM accounts");
+while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
+	$users[] = $row[0];
+	$open_friend[$row[0]] = 0;
+}
+
+/*
+Get how many open friendship each specific user holding by
+check the health of all friendship.
+OPEN => friendship is not broken and the user haven't confirmed yet
+*/
+$result = mysql_query("SELECT * FROM mingle_status");
+while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+	$curtime = gettimeofday(true);
+	$status = $row["mingle_status"];
+	switch ($status) {
+		case 1: // A confirms
+		case 2: // B confirms
+			if ($status == 1)
+				$idle_user = $row["user_b"];
+			else
+				$idle_user = $row["user_a"];
+			// If one of the users confirmed more than 60 secs ago,
+			// friendship is broken. Otherwise, it is open.
+			if ($curtime - $row["time"] <= 61.0) // not broken
+				$open_friend[$idle_user]++;
+			break;
+		case 3: // complete
+			break;
+		default: // open i.e. status = 0
+			$open_friend[$row["user_a"]]++;
+			$open_friend[$row["user_b"]]++;
+	}
+}
+
+/*
+Find new friends (to meet) for each user who has less than 5 friends.
+*/
+foreach ($users as $user_a)
+	if ($open_friend[$user_a] < 5)
+		foreach ($users as $user_b)
+			if ($open_friend[$user_b] < 5 && $user_b != $user_a) {
+				// build new friendship
+				$open_friend[$user_a]++;
+				$open_friend[$user_b]++;
+				// play with db
+				$curtime = gettimeofday(true);
+				mysql_query("INSERT INTO mingle_status (user_a, user_b, status, time) VALUES (" . $user_a . ", " . $user_b . ", 0, " . $curtime . ")");
+			}
+
+mysql_close($con);
+?>
