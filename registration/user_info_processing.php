822085be4fce254a585a3fbcfef5b32e4d5ce53e
Malathi Raghavan <malathi.raghavan@gmail.com>
2013-01-11 17:55:55 -0800

    User registration screen

---
+++ registration/user_info_processing.php
@@ -0,0 +1,26 @@
+<?php
+
+$pk_id = $_POST['pk_id'];
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$twitter = $_POST['twitter'];
+
+require_once("../config.php");
+$DB = new mysqli( DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE );
+
+$safe_first_name = $DB->real_escape_string($first_name);
+$safe_last_name = $DB->real_escape_string($last_name);
+$safe_email = $DB->real_escape_string($email);
+$safe_phone = $DB->real_escape_string($phone);
+$safe_twitter = $DB->real_escape_string($twitter);
+
+$query = 'UPDATE accounts SET first_name = "'.$safe_first_name.'", last_name = "'.$safe_last_name.'", email = "'.$safe_email.'", phone = "'.$safe_phone.'", twitter = "'.$safe_twitter.'" WHERE PK = "'.$pk_id.'"';
+
+echo $query;
+
+$DB->query($query);
+
+
+?>
