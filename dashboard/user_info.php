6920c10324ded0559352e20cdff9743aafc3545e
Arapat Jurat <arapat@arap.at>
2013-01-11 23:08:14 -0800

    Dashboard: get the total point for specific user

--- dashboard/user_info.php
+++ dashboard/user_info.php
@@ -8,10 +8,10 @@ mysql_select_db(DB_DATABASE, $con);
 $token = $_REQUEST["token"];
 $pgid = $_REQUEST["pgid"];
 
-$safe_token = $DB->real_escape_string($token);
-$safe_pgid = $DB->real_escape_string($pgid);
+$safe_token = mysql_real_escape_string($token);
+$safe_pgid = mysql_real_escape_string($pgid);
 
-$query = 'SELECT accounts_pk FROM accounts WHERE token = "'.$safe_token.'" AND pgid = "' . $safe_pgid . '"';
+$query = 'SELECT * FROM accounts WHERE token = "'.$safe_token.'" AND pgid = "' . $safe_pgid . '"';
 
 $result = mysql_query($query);
 $num_results = mysql_num_rows($result);
@@ -20,8 +20,27 @@ If $num_result=0, the token is incorrect.
 */
 
 $row = $result->fetch_assoc();
+$accounts_pk = $row['accounts_pk'];
 $first_name = $row['first_name'];
 $last_name = $row['last_name'];
 
 mysql_close($con);
+
+function get_total_points($pgid)
+	{
+		$con = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD) or
+		    die("Could not connect: " . mysql_error());
+		mysql_select_db(DB_DATABASE, $con);
+		
+		$safe_pgid = mysql_real_escape_string($pgid);
+		$query = 'SELECT points FROM points WHERE accounts_fk=' . $accounts_pk;
+		$result = mysql_query($query);
+		$row = $result->fetch_assoc();
+		$sum = 0;
+		foreach($row as $pt)
+			$sum += $pt;
+		return $sum;
+		
+		mysql_close($con);
+	}
 ?>
