0db2961354d29331e1250bbf784fca1e9d191e22
Eric Jang <ericjang2004@gmail.com>
2013-01-11 22:13:06 -0800

    fooasdf

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,22 +1,67 @@
 <?php
-// require_once("../config.php");
-// $DB = new mysqli( DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE );
-//
-// //get params
-// $token_id = $_GET["token"];
-// $pgid = $_GET["pgid"];
-//
-// $query = 'SELECT first_name, last_name FROM accounts WHERE pgid = ' . $pgid;
-// $result = $DB->query($query);
-// $row = $result->fetch_assoc();
-// $phone_number = $row['phone_number'];
-//
-//
-// $safe_token = $DB->real_escape_string($token_id);
-// $safe_pgid = $DB->real_escape_string($pgid);
-//
-// $query = 'SELECT accounts_pk FROM accounts WHERE token = "'.$safe_token.'" AND pgid = "' . $safe_pgid . '"';
+require_once("../config.php");
+$DB = new mysqli( DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE );
 
+//get params
+$token_id = $_GET["token"];
+$pgid = $_GET["pgid"];
+
+$query = 'SELECT first_name, last_name FROM accounts WHERE pgid = ' . $pgid;
+$result = $DB->query($query);
+$row = $result->fetch_assoc();
+$phone_number = $row['phone_number'];
+
+
+$safe_token = $DB->real_escape_string($token_id);
+$safe_pgid = $DB->real_escape_string($pgid);
+
+$query = 'SELECT accounts_pk FROM accounts WHERE token = "'.$safe_token.'" AND pgid = "' . $safe_pgid . '"';
+
+function is_open($pgid, $mingle)
+	{
+		// open friendship
+		if ($mingle["status"] == 0)
+			return true;
+
+		// friendship waiting for confirmation
+		$curtime = gettimeofday(true);
+		if ($mingle["status"] < 3 && $curtime - $mingle["time"] <= 61) {
+			if ($pgid == $mingle["user_a"] && $mingle["status"] == 2)
+				return true;
+			if ($pgid == $mingle["user_b"] && $mingle["status"] == 1)
+				return true;
+		}
+
+		// non-open
+		return false;
+	}
+
+require_once("../config.php");
+$con = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD) or
+    die("Could not connect: " . mysql_error());
+mysql_select_db(DB_DATABASE, $con);
+
+
+// Get all of the mingles involve current user.
+$pgid = $_REQUEST["pgid"];
+$result = mysql_query( "SELECT * FROM mingle_status WHERE user_a=" . $pgid . " OR user_b=" . $pgid);
+// $friends is what you want
+while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+	// eliminate non-open friendship
+	if (is_open($pgid, $row))
+		continue;
+
+	if ($row["user_a"] == $pgid)
+		$friend_id = $row["user_b"];
+	else
+		$friend_id = $row["user_a"];
+
+	$frind_result = mysql_query("SELECT first_name, last_name, intro FROM accounts WHERE pgid=" . $friend_id);
+	$friend_info = mysql_fetch_array($frind_result, MYSQL_ASSOC);
+	$friends[] = array("pk" => $row["mingle_status_pk"], "pgid" => $friend_id, "first_name" => $friend_info["first_name"], "last_name" => $friend_info["last_name"], "info" => $friend_info["intro"]);
+}
+
+mysql_close($con);
 ?>
 
 
@@ -61,22 +106,15 @@
     <div id="leaderboard">
       <h2>Leaderboard</h2>
       <table>
-        <!--some php required-->
-          <!-- <tr>
-                        <td>1st</td>
-                        <td>Jerry</td>
-                        <td>100</td>
-                    </tr>
-                    <tr>
-                        <td>2nd</td>
-                        <td>Molly</td>
-                        <td>40</td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>Chuck</td>
-                        <td>10</td>
-                    </tr> -->
+        <?php
+          foreach($highscores as $highscore) {
+            <tr>
+              <td>$highscore['rank']</td>
+              <td>$highscore['firstname'] . $highscore['lastname']</td>
+              <td>$highscore['points']</td>
+            </tr>
+          }
+        ?>
       </table>
     </div>
 
@@ -84,15 +122,15 @@
       <h2>Play Mingle</h2>
       <fieldset data-role="controlgroup">
       <legend>Find and say hello to these people! They will be looking for you as well. Open their info box and click their check box when you meet them!</legend>
-
       <ul data-role="listview" data-inset="true">
-        <li><a class="minglename" href="/dashboard/userProfile.php?userID=1" data-transition="slide">Acura</a></li>
-        <li><a class="minglename" href="/dashboard/userProfile.php?userID=1" data-transition="slide">Audi</a></li>
-        <li><a class="minglename" href="/dashboard/userProfile.php?userID=1" data-transition="slide">BMW</a></li>
-        <li><a class="minglename" href="/dashboard/userProfile.php?userID=1" data-transition="slide">Cadillac</a></li>
-        <li><a class="minglename" href="/dashboard/userProfile.php?userID=1" data-transition="slide">Ferrari</a></li>
+        <?php
+          foreach($friends as $friend) {
+            <li>
+              <a href="/dashboard/userProfile.php?userID="+$currentUser['pgid']+"&token="+$token+"&friendID="+$friend['pgid'] data-transition="slide">$friend['firstname'] . $friend['lastname']</a>
+            </li>
+          }
+        ?>
       </ul>
-
     </div>
 
   </div><!-- /content -->
