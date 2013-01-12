fb885df045cc71c1e566bc931107329fdf326b5d
Matt McNamara <matt@expensify.com>
2013-01-12 12:02:01 -0800

    Apply fix.

--- dashboard/apply.php
+++ dashboard/apply.php
@@ -9,7 +9,8 @@ $safe_pgid = $DB->real_escape_string( $_REQUEST["pgid"] );
 
 $selectQuery = 'SELECT count(1) from points inner join accounts on accounts.accounts_pk = points.accounts_fk where pgid=' . $safe_pgid .' and token = "' . $safe_token .'" and reason = "You made the right move."';
 $result = $DB->query( $selectQuery );
-if( $result->num_results == 0 ){
+if( $result->num_rows == 0 ){
+    pimplog( 'Lucky user just made the right choice.  THE DAMN RIGHT CHOICE IF I DO SAY.' );
     $insertQuery = 'INSERT INTO points( accounts_fk, points, reason, created ) VALUES( (SELECT accounts_pk FROM accounts where pgid = ' . $safe_pgid .' and token = "' . $safe_token . '"), ' . JOBS . ', "You made the right move.", CURRENT_TIMESTAMP() ';
     $result = $DB->query( $insertQuery );
 }
