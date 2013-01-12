9698cc0885c9d44ad0ba23addb5f31f584ccb55c
Salvatore Testa <sal.testa@gmail.com>
2013-01-11 19:20:12 -0800

    twilio function text_person(pk,message) added

--- twilio/twilio.php
+++ twilio/twilio.php
@@ -1,17 +1,31 @@
 <?php
-require('twilio-php/Services/Twilio.php');
+require_once('twilio-php/Services/Twilio.php');
+require_once("../config.php");
 
-$sid = "AC9f9728ff495a697fdcd98a09ea005220"; // Your Account SID from www.twilio.com/user/account
-$token = "42fbce0cb806c1f4d5e1a1388e9311bd"; // Your Auth Token from www.twilio.com/user/account
-$client = new Services_Twilio($sid, $token);
-
+/*
+ * Send a text message to a person given their PK
+ *
+ * ex. text_person(7,"Hello World!");
+ */
 function text_person($pk,$text) {
+	/* Prepare an insert statement */
+	$sid = "AC9f9728ff495a697fdcd98a09ea005220"; // Your Account SID from www.twilio.com/user/account
+	$token = "42fbce0cb806c1f4d5e1a1388e9311bd"; // Your Auth Token from www.twilio.com/user/account
+	$client = new Services_Twilio($sid, $token);
+	$DB = new mysqli( DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE );
+
+
+	/* Query their phone number */
+	$query = 'SELECT phone_number FROM accounts WHERE PK = '.$pk;
+	$result = $DB->query($query);
+	$row = $result->fetch_assoc();
+	$phone_number = $row['phone_number'];
+
+
 	$message = $client->account->sms_messages->create(
 		'4159443971', // From a valid Twilio number
-		'5124849744', // Text this number
+		$phone_number, // Text this number
 		$text
 	);
-	
 }
-
 ?>
