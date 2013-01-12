257eb9a0720dc8abc1263df9a4173c2383df0ef9
Eric Jang <ericjang2004@gmail.com>
2013-01-12 04:49:54 -0800

    fixed css link

--- dashboard/index.php
+++ dashboard/index.php
@@ -13,7 +13,7 @@ if ($play_mingle == 0) {
 <head>
   <title>User Dashboard</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="/css/jquery.mobile.css"/>
+  <link rel="stylesheet" href="/css/jquery.mobile.min.css"/>
   <script src="/js/jquery-latest.js"></script>
   <script src="/js/jquery.mobile-1.2.0.min.js"></script>
 </head>
@@ -104,7 +104,7 @@ if ($play_mingle == 0) {
       <ul data-role="listview" data-inset="true">
         <li>
           <?php
-            $account = get_user_info($pgid); 
+            $account = get_user_info($pgid);
             echo "<a href='/dashboard/roulette.php?pgid=" . $pgid . "&token=" . $token . "&pkid=".$account['accounts_pk']."' data-transition='slide' data-ajax='false'>Play Roulette</a>";
           ?>
         </li>
