3523d756bd712765a75248e8f2eb36230e224d10
Thach Hoang <thach@thachhoang.net>
2013-01-11 23:15:43 -0800

    Added AWS SDK, file uploader.
    Need bucket permission.

--- uploader/index.php
+++ uploader/index.php
@@ -1,28 +1,68 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Bootstrap 101 Template</title>
-	<!-- Bootstrap -->
+	<title>Uploader</title>
 	<link href="/css/bootstrap.min.css" rel="stylesheet" media="screen">
 </head>
 <body>
-	<?php
-		if (isset($_FILES["file"])){
-			if ($_FILES["file"]["error"] > 0){
-				echo "Error: " . $_FILES["file"]["error"] . "<br>";
+    <?php
+		if( !file_exists( '../config.php' ) ){
+			die( 'Config file doesn\' exist. Did you forget to copy config.php.default to config.php?');
+		}
+		
+		require_once( '../config.php' );
+		require_once '../libs/sdk-1.5.17.1/sdk.class.php';
+		
+		$bucket = S3_BUCKET; //."_test";
+		$allowedExts = array("jpg", "jpeg", "gif", "png");
+		
+		if (isset($_FILES["file"])) {
+		
+			$name = $_FILES["file"]["name"];
+			$name_tokens = explode(".", $name);
+			$extension = end($name_tokens);
+			
+			if ((($_FILES["file"]["type"] == "image/gif") || ($_FILES["file"]["type"] == "image/jpeg")
+					|| ($_FILES["file"]["type"] == "image/png") || ($_FILES["file"]["type"] == "image/pjpeg"))
+					&& in_array($extension, $allowedExts)) {
+					
+				if ($_FILES["file"]["error"] > 0) {
+					echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+				} else {
+					//echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+					//echo "Type: " . $_FILES["file"]["type"] . "<br>";
+					//echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+					//echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+					
+					// Instantiate the class
+					$s3 = new AmazonS3();
+					
+					// Create object from a file
+					$response = $s3->create_object(
+						$bucket,
+						$name,
+						array(
+							'fileUpload' => $_FILES['file']['tmp_name'],
+							'acl' => AmazonS3::ACL_PUBLIC
+							)
+						);
+
+					// Success?
+					if ($response->isOK()) {
+						echo "<img src=\"https://s3.amazonaws.com/".$bucket."/".$name."\" />";
+					}
+				}
 			} else {
-				echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-				echo "Type: " . $_FILES["file"]["type"] . "<br>";
-				echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-				echo "Stored in: " . $_FILES["file"]["tmp_name"];
+				echo "Invalid file";
 			}
 		}
 	?>
+	
 	<div class="container-fluid">
 		<div class="row-fluid">
 			<div class="span4">
 				<!--Sidebar content-->
-				<form enctype="multipart/form-data" action="index.php" method="post">
+				<form enctype="multipart/form-data" action="index2.php" method="post">
 					<input type="file" name="file" />
 					<input type="submit" value="Upload" />
 				</form>
